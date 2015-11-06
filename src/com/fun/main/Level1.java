b3607114725ec38fa34938938b21bf27004c00a6
984238 <984238@201-LB163-297.mcpsmd.org>
2015-11-06 13:14:09 -0500

    Added a moving platform, but is still very buggy. Refuses to spawn anywhere but the upper left corner, does not change y position, and freezes the game upon player contact with the side of the platform

--- src/com/fun/main/Level1.java
+++ src/com/fun/main/Level1.java
@@ -12,17 +12,18 @@ public class Level1 {
 		this.handler=handler;
 
 		handler.addObject(new Player(WIDTH/2, HEIGHT/2, ID.Player, handler));
-		handler.addObject(new BasicEnemy(r.nextInt(WIDTH/2), HEIGHT/2, ID.Enemy,handler));
+		/*handler.addObject(new BasicEnemy(r.nextInt(WIDTH/2), HEIGHT/2, ID.Enemy,handler));
 		handler.addObject(new BasicEnemy(r.nextInt(WIDTH/2), HEIGHT/2, ID.Enemy,handler));
 		handler.addObject(new BasicGreenEnemy(r.nextInt(WIDTH/2), HEIGHT/2, ID.Enemy,handler));
 		handler.addObject(new BasicGreenEnemy(r.nextInt(WIDTH/2), HEIGHT/2, ID.Enemy,handler));
 		handler.addObject(new BasicEnemy(r.nextInt(WIDTH/2)+WIDTH/2, HEIGHT/2, ID.Enemy,handler));
-		//handler.addObject(new BasicEnemy(r.nextInt(WIDTH/4), HEIGHT/4, ID.Enemy));
+		*///handler.addObject(new BasicEnemy(r.nextInt(WIDTH/4), HEIGHT/4, ID.Enemy));
 		handler.addObject(new BasicWall(WIDTH-300, (int) ((int) HEIGHT/1.5), ID.Wall, 100,400));
 		handler.addObject(new BasicWall(0, HEIGHT-100, ID.Wall, 720,400));
 		handler.addObject(new BasicWall(WIDTH-100, (int) ((int) HEIGHT/2), ID.Wall, 100,100));
 		handler.addObject(new BasicEnemy(50, 50, ID.Enemy, handler));
 		handler.addObject(new BasicBlueEnemy(55,55, ID.Enemy, handler));
+		handler.addObject(new MovingWall(5, 5, ID.Wall, 100, 30, 500, 10, 2, handler));
 	}
 	public KeyInput returnKeyInput(){
 		return new KeyInput(handler);
